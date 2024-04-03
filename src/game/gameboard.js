f6704957ea7c94e641161ef62edd334f843d5c21
root <root@Laptop-Ehsan>
2024-04-04 01:15:48 +0330

    Write receive attack function

--- src/game/gameboard.js
+++ src/game/gameboard.js
@@ -3,6 +3,7 @@ class Gameboard {
     this.grid = Array(10)
       .fill(10)
       .map(() => Array(10).fill(0));
+    this.ships = { c: null, b: null, d: null, s: null, p: null };
   }
 
   placeShip(coordinates, name) {
@@ -10,5 +11,13 @@ class Gameboard {
       const [x, y] = co;
       this.grid[x][y] = name;
     });
+  }
+
+  receiveAttack(x, y) {
+    if (this.grid[x][y] === 0) {
+      this.grid[x][y] = 1;
+    } else {
+      this.ships[this.grid[x][y]].hit();
+    }
   }
 }
