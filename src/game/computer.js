705784d156e14a19818916647d3ba806e20f06fe
root <root@Laptop-Ehsan>
2024-04-06 00:22:03 +0330

    Make computer attack random coordinates

---
+++ src/game/computer.js
@@ -0,0 +1,27 @@
+class computer {
+  constructor(enemyBoard) {
+    this.enemyBoard = enemyBoard;
+    this.shots = new Set();
+  }
+
+  attackEnemy() {
+    let [x, y] = this.attackRandom();
+    this.enemyBoard.receiveAttack(x, y);
+  }
+
+  hasLost() {
+    return this.enemyBoard.allSunk();
+  }
+
+  attackRandom() {
+    let random = () => Math.floor(Math.random() * 10);
+    let x = random();
+    let y = random();
+    while (this.shots.has(`${x}${y}`)) {
+      x = random();
+      y = random();
+    }
+    this.shots.add(`${x}${y}`);
+    return [x, y];
+  }
+}
